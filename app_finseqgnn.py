e789266c252f20d1b31e993831d335aabd339392
Srikanta <adirajusrikantasharma@gmail.com>
2026-04-04 05:15:25 +0530

    Update app_finseqgnn.py

--- app_finseqgnn.py
+++ app_finseqgnn.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
 
-st.title("📊 Real Time Stock Dashboard (DeepCharts Style)")
+st.title("📊 Real Time Stock Dashboard + (Z-Score Prediction)")
 
 # ==============================
 # SAFE VALUE
